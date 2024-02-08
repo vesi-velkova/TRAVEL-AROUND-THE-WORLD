2825e487e811d90c45ad856629f1946d3112d72e
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-08 22:16:40 +0200

    Login required, redirect after login to welcome page

--- traveling_the_world/gui/views.py
+++ traveling_the_world/gui/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
 def index(request):
     """Welcome page."""
     return HttpResponse('Welcome!')
