59f302e139bbfac1b2a872595e6ae81514ea32d4
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-08 22:20:57 +0200

    Added logout url and settings

--- traveling_the_world/gui/urls.py
+++ traveling_the_world/gui/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index), # Map empty url to the Welcome view.
-    path('login', auth_views.LoginView.as_view()) # Add login url.
+    path('login', auth_views.LoginView.as_view()), # Add login url.
+    path('logout', auth_views.LogoutView.as_view()) # Add logout url.
 ]
