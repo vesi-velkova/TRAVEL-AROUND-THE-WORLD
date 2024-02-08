b23ad9f95f4e0cae7a24db4bdbd9c038f1c7d7cf
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-08 22:03:44 +0200

    Add login url

--- traveling_the_world/gui/urls.py
+++ traveling_the_world/gui/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index), # Map empty url to the Welcome view.
+    path('login', auth_views.LoginView.as_view()) # Add login url.
 ]
