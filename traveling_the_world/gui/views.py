e5693c21ba26f26cdb26457506b155060ce5e579
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-09 20:46:44 +0200

    Fix logout url and view

--- traveling_the_world/gui/views.py
+++ traveling_the_world/gui/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
-@login_required(login_url='/login')
+@login_required(login_url='/login/')
 def index(request):
     """Welcome page."""
     return render(request, 'index.html')
 
-@login_required(login_url='/login')
 def logout_view(request):
     """
     Logout view. Login is required. 
     Redirects to the login page. 
     """
+    #print(request.user)
     logout(request)
-    return redirect('/login')
+    #print(request.user)
+    return redirect("/login/")
