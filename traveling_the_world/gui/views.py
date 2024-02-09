04d0a82416728e3838c98d7abcf0ccb129e9a542
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-09 18:50:46 +0200

    Add logout view

--- traveling_the_world/gui/views.py
+++ traveling_the_world/gui/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 @login_required(login_url='/login')
 def index(request):
     """Welcome page."""
-    return HttpResponse('Welcome!')
+    return render(request, 'index.html')
+
+@login_required(login_url='/login')
+def logout_view(request):
+    """
+    Logout view. Login is required. 
+    Redirects to the login page. 
+    """
+    logout(request)
+    return redirect('/login')
