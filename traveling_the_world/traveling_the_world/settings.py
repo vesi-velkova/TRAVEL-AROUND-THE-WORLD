e7f4d233e9dcb82fa842a5449bfd50d0ca6f7335
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-09 20:47:58 +0200

    Add logout_redirect and login_url to settings

--- traveling_the_world/traveling_the_world/settings.py
+++ traveling_the_world/traveling_the_world/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_URL = 'login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -125,4 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 
-LOGOUT_REDIRECT_URL = '/login'
+LOGOUT_REDIRECT_URL = '/login/'
