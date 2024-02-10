c339be76c250c55ab07e1f5e8073c4c998626d9e
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-10 21:44:07 +0200

    Separate the main pages in a class inside the views.py

--- traveling_the_world/gui/views.py
+++ traveling_the_world/gui/views.py
@@ -3,24 +3,25 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .models import DreamDestinationsList
 
-@login_required(login_url='/login/')
-def home_page(request):
-    """Welcome page."""
-    return render(request, 'home_page.html')
+class MainPagesViews:
+    @login_required(login_url='/login/')
+    def home_page(request):
+        """Welcome page."""
+        return render(request, 'home_page.html')
 
-@login_required(login_url='/login/')
-def dream_destinations_view(request):
-    """Dream destinations page."""
-    context = {
-        'dream_destination_lists': DreamDestinationsList.objects.filter(owner=request.user)
-    }
-    return render(request, 'dream_list.html', context)
+    @login_required(login_url='/login/')
+    def dream_destinations_view(request):
+        """Dream destinations page."""
+        context = {
+            'dream_destination_lists': DreamDestinationsList.objects.filter(owner=request.user)
+        }
+        return render(request, 'dream_list.html', context)
 
-@login_required(login_url='/login/')
-def logout_view(request):
-    """
-    Logout view. Login is required. 
-    Redirects to the login page. 
-    """
-    logout(request)
-    return redirect("/login/")
+    @login_required(login_url='/login/')
+    def logout_view(request):
+        """
+        Logout view. Login is required. 
+        Redirects to the login page. 
+        """
+        logout(request)
+        return redirect("/login/")
