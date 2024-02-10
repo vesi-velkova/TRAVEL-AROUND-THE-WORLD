c47c49015ed45a135c6e5e5a25ab2be6d5d301ca
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-10 20:39:23 +0200

    Dream destinations lists is visible from the welcome page

--- traveling_the_world/gui/views.py
+++ traveling_the_world/gui/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from .models import DreamDestinationsList
 
 @login_required(login_url='/login/')
 def index(request):
     """Welcome page."""
-    return render(request, 'index.html')
+    context = {
+        'dream_destination_lists': DreamDestinationsList.objects.filter(owner=request.user)
+    }
+    return render(request, 'index.html', context)
 
+@login_required(login_url='/login/')
 def logout_view(request):
     """
     Logout view. Login is required. 
     Redirects to the login page. 
     """
-    #print(request.user)
     logout(request)
-    #print(request.user)
     return redirect("/login/")
