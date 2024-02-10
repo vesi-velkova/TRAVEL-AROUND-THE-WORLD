08cfddb10b4192e8cc0da924f355d32a746e5244
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-10 14:02:12 +0200

    Add dream destinations model

--- traveling_the_world/gui/models.py
+++ traveling_the_world/gui/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class DreamDestinations(models.Model):
+    destination_name = models.CharField(max_length=90) 
+    # The city with longest name is Taumatawhakatangihangakoauauotamate 
+    # aturipukakapiki-maungahoronukupokaiwhenuakitnatahu (85 chars length)
+    country = models.CharField(max_length=50)
+    # Country with longest name is United Kingdom of Great Britain and Northern Ireland (46 letters).
