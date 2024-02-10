42d88da1ea7095c62dbd728e28d0e98470c0a844
veselina-velkova <veselina.velkova@strypes.eu>
2024-02-10 14:51:43 +0200

    Add list for all of the dream destinations

--- traveling_the_world/gui/admin.py
+++ traveling_the_world/gui/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DreamDestinations
+from .models import DreamDestinations, DreamDestinationsList
 
 class DreamDestinationsAdmin(admin.ModelAdmin):
     model = DreamDestinations
@@ -8,4 +8,14 @@ class DreamDestinationsAdmin(admin.ModelAdmin):
     list_display = ('destination_name', 'country')
     fields = ('destination_name', 'country')
 
+
+class DreamDestinationsListAdmin(admin.ModelAdmin):
+    model = DreamDestinationsList
+    ordering = ('dream_destinations_list', 'owner')
+    search_fields = ('dream_destinations_list',)
+    list_display = ('dream_destinations_list', 'owner')
+    fields = ('dream_destinations_list', 'owner')
+
+
 admin.site.register(DreamDestinations, DreamDestinationsAdmin)
+admin.site.register(DreamDestinationsList, DreamDestinationsListAdmin)
--- traveling_the_world/gui/models.py
+++ traveling_the_world/gui/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class DreamDestinations(models.Model):
     destination_name = models.CharField(max_length=90) 
@@ -6,3 +7,8 @@ class DreamDestinations(models.Model):
     # aturipukakapiki-maungahoronukupokaiwhenuakitnatahu (85 chars length)
     country = models.CharField(max_length=50)
     # Country with longest name is United Kingdom of Great Britain and Northern Ireland (46 letters).
+    
+
+class DreamDestinationsList(models.Model):
+    dream_destinations_list = models.CharField(max_length = 50, default = "DREAM DESTINATIONS")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
